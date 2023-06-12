b542fbf6ba75c5b7f904e8c6a55bb6ced4c77510
ericz02 <ezheng0720@gmail.com>
2023-06-12 15:01:44 -0400

    fixed images not loading

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -22,7 +22,7 @@ const SongItem: React.FC<SongItemProps> = ({ data, onClick}) => {
             className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 
             bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3">
 
-            <div className="relative aspect-square w-fullh-full rounded-md overflow-hidden">
+            <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
 
                 <Image
                     className="object-cover"
