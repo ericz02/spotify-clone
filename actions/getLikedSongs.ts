d2ac8bd9b05e29ebf53f143fe38c9e415ef361e9
ericz02 <ezheng0720@gmail.com>
2023-06-20 09:58:03 -0400

    fixed server error liked songs not rendering

--- actions/getLikedSongs.ts
+++ actions/getLikedSongs.ts
@@ -1,33 +1,30 @@
 import { Song } from "@/types";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import error from "next/error";
 import { cookies } from "next/headers";
 
 const getLikedSongs = async (): Promise<Song[]> => {
     const supabase = createServerComponentClient({
-        cookies: cookies
+        cookies: cookies,
     });
 
-    const { data: { session }, } = await supabase.auth.getSession();
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
 
     const { data } = await supabase
-        .from('liked_songs')
-        .select('*, songs(*)')
-        .eq('user_id', session?.user?.id)
-        .order('created_at', { ascending: false })
+        .from("liked_songs")
+        .select("*, songs(*)")
+        .eq("user_id", session?.user?.id)
+        .order("created_at", { ascending: false });
 
-        if (error) {
-            console.log(error);
-            return [];
-        }
+    if (!data) return [];
 
-    if (!data) {
-        return [];
-    }
+    // removed checking error
 
     return data.map((item) => ({
-        ...item.songs
-    }))
+        ...item.songs,
+    }));
 };
+
 
 export default getLikedSongs;
